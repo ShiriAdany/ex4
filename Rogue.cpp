fc17080a5e0c79f7ae30814d02a5981219c068bc
ShiriAdany <102622677+ShiriAdany@users.noreply.github.com>
2022-06-07 13:54:47 +0300

    implementation

--- Rogue.cpp
+++ Rogue.cpp
@@ -2,3 +2,19 @@
 // Created by shiri on 06/06/2022.
 //
 
+#include "Rogue.h"
+using std::string;
+
+Rogue::Rogue(string name) {
+    Player{name};
+}
+
+Rogue::addCoins(int coins)
+{
+    if(coins>0)
+    {
+        m_coins += (2*coins);
+    }
+}
+
+

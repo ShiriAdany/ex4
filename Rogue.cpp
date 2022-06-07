4f0bd564ffdd6fe29d18d0ac48b3191ae8a42454
ShiriAdany <102622677+ShiriAdany@users.noreply.github.com>
2022-06-07 20:44:51 +0300

    m_job in printInfo

---
+++ Rogue.cpp
@@ -0,0 +1,25 @@
+//
+// Created by shiri on 06/06/2022.
+//
+
+#include "Rogue.h"
+using std::string;
+
+Rogue::Rogue(string name) {
+    Player{name};
+}
+
+Rogue::addCoins(int coins)
+{
+    if(coins>0)
+    {
+        m_coins += (2*coins);
+    }
+}
+
+void Rogue::printInfo() const {
+    std::ostream stream;
+    printPlayerDetails(stream,m_name, m_job,m_level,m_force,m_HP,m_coins);
+}
+
+

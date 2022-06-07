ca6d0fb364f42543abe105061e639ef2f08d3b01
ShiriAdany <102622677+ShiriAdany@users.noreply.github.com>
2022-06-07 17:32:35 +0300

    Add files via upload

--- Merchant.cpp
+++ Merchant.cpp
@@ -2,3 +2,71 @@
 // Created by shiri on 06/06/2022.
 //
 
+#include "Merchant.h"
+using std::stoi;
+
+int Merchant::getPlayerChoice(Player &player) {
+    string input;
+    int choice;
+
+    bool endTurn = false;
+    do{
+        cin.getline(input, 1);
+        choice = stoi(input);
+        if(choice >= action::Options || choice < 0)
+        {
+            printInvalidInput();
+        }
+        else{
+            endTurn = true;
+        }
+    }while(!endTurn);
+
+    return choice;
+
+}
+
+
+void Merchant::makeTrade(Player &player) {
+    std::ostream stream;
+    printMerchantInitialMessageForInteractiveEncounter(stream,Player &player,player.m_coins);
+    int choice = getPlayerChoice(player);
+    int cost =0 ;
+
+    switch(choice){
+        case action::Nothing:
+        {
+            break;
+        }
+
+        case action::Health_Potion:
+        {
+            if(player.pay(HEALTH_POTION_COST))
+            {
+                cost = HEALTH_POTION_COST;
+                player.heal(1);
+            }
+            else{
+                printMerchantInsufficientCoins(stream);
+            }
+            break;
+        }
+
+        case action::Force_Boost:
+        {
+            if(player.pay(FORCE_BOOST_COST))
+            {
+                cost = FORCE_BOOST_COST;
+                player.buff(1);
+            }
+            else{
+                printMerchantInsufficientCoins(stream);
+            }
+            break;
+        }
+        default:
+            break;
+    }
+    printMerchantSummary(stream,player.m_name,choice,cost);
+}
+
--- Merchant.h
+++ Merchant.h
@@ -5,4 +5,27 @@
 #ifndef EX4_MERCHANT_H
 #define EX4_MERCHANT_H
 
+#include "Card.h"
+
+enum class Action {Nothing, Health_Potion, Force_Boost, Options};
+
+class Merchant : public Card{
+
+public:
+    Merchant();
+
+    int getPlayerChoice(Player &player);
+
+    void makeTrade(Player &player);
+
+    Merchant(const Merchant& other) = default;
+    ~Merchant() = default;
+    Merchant& operator=(const Merchant& other) = default;
+
+private:
+    static int HEALTH_POTION_COST 5;
+    static int FORCE_BOOST_COST 10;
+};
+
+
 #endif //EX4_MERCHANT_H

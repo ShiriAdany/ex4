2f32d20d3b6c016de958c01f704bb17448fe6d16
ShiriAdany <102622677+ShiriAdany@users.noreply.github.com>
2022-06-09 01:44:05 +0300

    3 exception classes

--- Exception.h
+++ Exception.h
@@ -9,11 +9,45 @@
 
 class Exception: public std::exception
 {
-    class DeckFileNotFound{};
-
-    class DeckFileFormatError{};
-
-    class DeckFileInvalidSize{};
-
+public:
+    const char* what() const override{
+        return exception::what();
+    };
 };
+
+
+class DeckFileNotFound : public Exception{
+public:
+    const char* what() const override
+    {
+        return "Deck File Error: File not found";
+    };
+};
+
+
+class DeckFileInvalidSize: public Exception{
+public:
+    const char* what() const override
+    {
+        return "Deck File Error: Deck size is invalid";
+    };
+};
+
+
+
+class DeckFileFormatError: public Exception{
+public:
+    DeckFileFormatError(int line): m_line(line){};
+
+    const char* what() const override{
+        std::string message = "Deck File Error: File format error in line " + std::to_string(m_line);
+        return message.c_str();
+    }
+
+private:
+    int m_line;
+};
+
+
+
 #endif //EX4_EXCEPTION_H

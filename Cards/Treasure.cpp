1a02504b3dcd285558fbdfaba3417f41cfe645ab
ShiriAdany <102622677+ShiriAdany@users.noreply.github.com>
2022-06-08 14:40:08 +0300

    Add files via upload

--- Cards/Treasure.cpp
+++ Cards/Treasure.cpp
@@ -2,3 +2,15 @@
 // Created by shiri on 06/06/2022.
 //
 
+#include "Treasure.h"
+
+Treasure::Treasure() :Card(CardType::Treasure){
+    m_coins = DEFAULT_COINS;
+}
+
+void Treasure::applyEncounter(Player &player) {
+    player.addCoins(m_coins);
+    printTreasureMessage();
+}
+
+
--- Cards/Treasure.h
+++ Cards/Treasure.h
@@ -5,4 +5,21 @@
 #ifndef EX4_TREASURE_H
 #define EX4_TREASURE_H
 
+#include "Card.h"
+
+class Treasure : public Card{
+public:
+    Treasure();
+
+    void applyEncounter(Player& player) override;
+
+    Treasure(const Treasure& other) = default;
+    ~Treasure() = default;
+    Treasure& operator=(const Treasure& other) = default;
+
+private:
+    int m_coins;
+
+    static const int DEFAULT_COINS = 10;
+};
 #endif //EX4_TREASURE_H

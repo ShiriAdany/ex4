a7de1e57247722c4da08a5c1a0e26b47f40ae89f
ShiriAdany <102622677+ShiriAdany@users.noreply.github.com>
2022-06-12 14:11:55 +0300

    constructor

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -2,3 +2,177 @@
 // Created by shiri on 06/06/2022.
 //
 
+#include "Mtmchkin.h"
+using std::ifstream;
+using std::string;
+using std::cin;
+
+Mtmchkin::Mtmchkin(const string fileName) {
+    printStartGameMessage();
+
+    initiateDeck(fileName);
+    initiatePlayers();
+
+}
+
+void Mtmchkin::initiateDeck(const string fileName) {
+
+    ifstream source(fileName);
+    if(!source)
+    {
+        //throw exception
+    }
+
+    char line[256];
+    while(source.getline(line, sizeof(line)))
+    {
+        string cardName = line;
+        Card* newCard;
+        if(cardName == "Goblin")
+        {
+            newCard = new Goblin();
+        }
+        else if(cardName == "Vampire")
+        {
+            newCard = new Vampire();
+        }
+        else if(cardName == "Dragon")
+        {
+            newCard = new Dragon();
+        }
+        else if(cardName == "Merchant")
+        {
+            newCard = new Merchant();
+        }
+        else if(cardName == "Treasure")
+        {
+            newCard = new Treasure();
+        }
+        else if(cardName == "Pitfall")
+        {
+            newCard = new Pitfall();
+        }
+        else if(cardName == "Barfight")
+        {
+            newCard = new Barfight();
+        }
+        else if(cardName == "Fairy")
+        {
+            newCard = new Fairy();
+        }
+        else
+        {
+            //throw error of unknown card type
+        }
+        m_deck.push(newCard);
+    }
+}
+
+void Mtmchkin::initiatePlayers() {
+    printEnterTeamSizeMessage();
+    m_numberOfPlayers = getNumberOfPlayers();
+
+    std::vector<string> words;
+    bool done = false;
+    for(int i =0; i<m_numberOfPlayers; i++) {
+        while(!done)
+        {
+            words.clear();
+            words = getPlayerArguments();
+            done = isValidArguments(words);
+        }
+    }
+
+}
+
+bool Mtmchkin::isValidArguments(std::vector<string> words)
+{
+    string playerName = words[0];
+    string playerClass = words[1];
+
+    if (validName(playerName)) {
+        if (playerClass == "Rogue") {
+            m_playersQueue.push(new Rogue(playerName));
+        } else if (playerClass == "Fighter") {
+            m_playersQueue.push(new Fighter(playerName));
+        } else if (playerClass == "Wizard") {
+            m_playersQueue.push(new Wizard(playerName));
+        } else {
+            printInvalidClass();
+            return false;
+        }
+    } else {
+        printInvalidName();
+        return false;
+    }
+
+    return true;
+
+}
+
+int Mtmchkin::getNumberOfPlayers() {
+    string input;
+    int numberOfPlayers;
+    bool valid = false;
+    do{
+        std::getline(cin, input);
+        numberOfPlayers = stoi(input);
+        if(isdigit(numberOfPlayers) && numberOfPlayers <= 6 && numberOfPlayers >= 2 )
+        {
+            valid = true;
+        }
+        printInvalidInput();
+    }while(!valid);
+
+    return numberOfPlayers;
+}
+
+std::vector<std::string> Mtmchkin::getPlayerArguments() {
+    printInsertPlayerMessage();
+
+    string input;
+    string argument;
+    std::vector<string> words;
+    bool valid = false;
+
+    do{
+        std::getline(cin,input);
+        std::istringstream seperateString(input);
+        while(seperateString >> argument)
+        {
+            words.push_back(argument);
+        }
+
+        if(words.size() == NUMBER_OF_ARGUMENTS)
+        {
+            valid = true;
+        }
+        else{
+            printInvalidNumberOfArgumens();
+            words.clear();
+        }
+    }while(!valid);
+
+    return words;
+}
+
+void Mtmchkin::printInvalidNumberOfArgumens() {
+    std::cout << "Please enter " << std::to_string(NUMBER_OF_ARGUMENTS) << " arguments" << std::endl;
+}
+
+bool Mtmchkin::validName(const std::string& name) {
+    if(name.size() >15)
+    {
+        return false;
+    }
+
+    for(char i : name){
+        if( !isalpha(i) || isspace(i)){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
--- Mtmchkin.h
+++ Mtmchkin.h
@@ -1,6 +1,27 @@
 #ifndef MTMCHKIN_H_
 #define MTMCHKIN_H_
 
+#include <string.h>
+#include "Exception.h"
+
+#include "Card.h"
+#include "Goblin.h"
+#include "Vampire.h"
+#include "Dragon.h"
+#include "Merchant.h"
+#include "Pitfall.h"
+#include "Barfight.h"
+#include "Fairy.h"
+#include "Treasure.h"
+
+#include "Rogue.h"
+#include "Fighter.h"
+#include "Wizard.h"
+
+#include <fstream>
+#include <queue>
+#include <vector>
+
 class Mtmchkin{
 
 public:
@@ -12,7 +33,7 @@ public:
     * @return
     *      A new instance of Mtmchkin.
     */
-    Mtmchkin(const string fileName);
+    Mtmchkin(const std::string fileName);
     
     /*
     * Play the next Round of the game - according to the instruction in the exercise document.
@@ -46,6 +67,21 @@ public:
     *          int - number of rounds played
     */
     int getNumberOfRounds() const;
+
+private:
+    std::queue<Card*> m_deck;
+    std::queue<Player*>  m_playersQueue;
+    int m_numberOfPlayers;
+
+    void initiateDeck(const std::string fileName);
+    void initiatePlayers();
+    static int getNumberOfPlayers();
+    static std::vector<std::string> getPlayerArguments();
+    static void printInvalidNumberOfArgumens();
+    bool isValidArguments(std::vector<std::string>);
+    static bool validName(const std::string& name);
+
+    static int const NUMBER_OF_ARGUMENTS = 2; //player's name and class
 };
 
 

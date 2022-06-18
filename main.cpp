4045e03a6ad17e903b8cf43ff9c4447635e5222a
ShiriAdany <102622677+ShiriAdany@users.noreply.github.com>
2022-06-18 14:39:12 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "Mtmchkin.h"
+
+
+int main(int argc, char** argv) {
+    try{
+        Mtmchkin game(argv[1]);
+        while(!game.isGameOver() && game.getNumberOfRounds() < 100){
+            game.playRound();
+            game.printLeaderBoard();
+        }
+    }
+    catch(...)
+    {
+        return 0;
+    }
+
     return 0;
 }
